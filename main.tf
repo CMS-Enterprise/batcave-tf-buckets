3463469e75ab01d1a800659b3792a448b8afbc64
Charles Bushong <bushong1@gmail.com>
2022-03-14 17:21:54 -0400

    BATIAI-286: Fixing trip for security hub (#1)

--- main.tf
+++ main.tf
@@ -21,8 +21,10 @@ resource "aws_s3_bucket_public_access_block" "landing_zone_buckets" {
   for_each = aws_s3_bucket.landing_zone_buckets
   bucket   = each.value.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls      = true
+  block_public_policy    = true
+  ignore_public_acls     = true
+ restrict_public_buckets = true
 }
 
 # data "aws_iam_policy_document" "bucket_policy" {

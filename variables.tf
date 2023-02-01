d13cb27d86322a18bedddcaf3d0588b19a942257
Gato <115658935+CollectiveUnicorn@users.noreply.github.com>
2023-01-31 19:10:14 -0500

    BATIAI-1194 - Add variable for server-side encryption (#5)
    Adds variable for server-side encryption so that AES256 can be specified when ADOs request it instead of aws:kms.

--- variables.tf
+++ variables.tf
@@ -17,3 +17,9 @@ variable "s3_bucket_kms_key_id" {
   default     = null
   description = "KMS Key used to encrypt s3 buckets.  Defaults to null, which uses default aws/s3 key"
 }
+
+variable "sse_algorithm" {
+  type        = string
+  default     = "aws:kms"
+  description = "The server-side encryption algorithm to use. Valid values are AES256 and aws:kms, defaults to aws:kms."
+}
